7ebac6079d06201a5b6d5d848de81802ed2a7234
BlazingSid <attarshahid62@gmail.com>
2026-08-03 11:38:05 +0530

    todays lecture

---
+++ data-structure/basic-questions1.py
@@ -0,0 +1,17 @@
+empty_stack = []
+
+#adding subjects
+empty_stack.append("maths")
+empty_stack.append("science")
+empty_stack.append("english")
+
+#printing
+print(empty_stack)
+
+#removing subjects
+empty_stack.pop()
+empty_stack.pop()
+empty_stack.pop()
+
+#printing
+print(empty_stack)
