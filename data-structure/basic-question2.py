b2352c6c631b9cef1ca7e723d9a1623d27e58f72
BlazingSid <attarshahid62@gmail.com>
2026-08-03 11:50:05 +0530

    3rd que

---
+++ data-structure/basic-question2.py
@@ -0,0 +1,23 @@
+cpu = []
+
+#adding processes
+cpu.append("P1")
+cpu.append("P2")
+cpu.append("P3")
+cpu.append("P4")
+
+#printing all
+print(cpu)
+
+#removing processes
+cpu.pop(0)
+print(cpu) #deletes P1
+
+cpu.pop(0) #deletes P2
+print(cpu) 
+
+cpu.pop(0) #deletes P3
+print(cpu) 
+
+cpu.pop(0) #deletes P4
+print(cpu) 
